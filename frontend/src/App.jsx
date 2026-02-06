f43e0f48e647823ab8e1a30f718c83009fe247d1
sigallage <sgallage0000@gmail.com>
2026-02-06 12:49:37 +0530

    tailwind and postcss installed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LandingPage from './pages/landingPage/landingPage.jsx'
 import RouteOptimizer from './pages/routeOptimizer/routeOptimizer.jsx'
 
 function App() {
-  return <RouteOptimizer />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/planner" element={<RouteOptimizer />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
