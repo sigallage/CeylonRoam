5a3bf61ce8cebab3134f44627831266981af3015
goagoonewardena <oshadhigoon@gmail.com>
2026-02-07 12:28:32 +0530

    Created the ResetPassword.jsx and ForgotPassword.jsx files

--- frontend/src/pages/routes/router.jsx
+++ frontend/src/pages/routes/router.jsx
@@ -1,6 +1,9 @@
 
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from '../Login/LoginPage.jsx';
+import ForgotPassword from '../Login/ForgotPassword.jsx';
+import ResetPassword from '../Login/ResetPassword.jsx';
+import SignUpPage from '../Login/SignUpPage.jsx';
 import LandingPage from '../landingPage/landingPage.jsx';
 import RouteOptimizer from '../routeOptimizer/routeOptimizer.jsx';
 
@@ -10,6 +13,9 @@ function Router() {
 			<Route path="/" element={<LandingPage />} />
 			<Route path="/planner" element={<RouteOptimizer />} />
 			<Route path="/login" element={<LoginPage />} />
+			<Route path="/reset-password" element={<ResetPassword />} />
+			<Route path="/forgot-password" element={<ForgotPassword />} />
+			<Route path="/signup" element={<SignUpPage />} />
 			{/* Add other routes here as needed */}
 		</Routes>
 	);
