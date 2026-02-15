0adfceb6632de311b90024d3590fe7d9ba2270a5
sigallage <sgallage0000@gmail.com>
2026-02-15 17:25:02 +0530

    refactor: Added a main layout

--- frontend/src/pages/routes/router.jsx
+++ frontend/src/pages/routes/router.jsx
@@ -1,5 +1,6 @@
 
 import { Routes, Route } from 'react-router-dom';
+import MainLayout from '../layout/mainLayout.jsx';
 import LoginPage from '../Login/LoginPage.jsx';
 import ForgotPassword from '../Login/ForgotPassword.jsx';
 import ResetPassword from '../Login/ResetPassword.jsx';
@@ -11,14 +12,18 @@ import VoiceTranslationPage from '../VoiceTranslation/VoiceTranslationPage.jsx';
 function Router() {
 	return (
 		<Routes>
+			{/* Landing page without header/footer */}
 			<Route path="/" element={<LandingPage />} />
-			<Route path="/planner" element={<RouteOptimizer />} />
-			<Route path="/login" element={<LoginPage />} />
-			<Route path="/reset-password" element={<ResetPassword />} />
-			<Route path="/forgot-password" element={<ForgotPassword />} />
-			<Route path="/signup" element={<SignUpPage />} />
-            <Route path="/voice-translation" element={<VoiceTranslationPage />} />
-			{/* Add other routes here as needed */}
+			
+			{/* All other pages with header/footer */}
+			<Route element={<MainLayout />}>
+				<Route path="/planner" element={<RouteOptimizer />} />
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/reset-password" element={<ResetPassword />} />
+				<Route path="/forgot-password" element={<ForgotPassword />} />
+				<Route path="/signup" element={<SignUpPage />} />
+				<Route path="/voice-translation" element={<VoiceTranslationPage />} />
+			</Route>
 		</Routes>
 	);
 }
