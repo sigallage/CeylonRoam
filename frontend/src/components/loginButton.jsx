a5af460dd1526ad58d875b80219f1a9ee151f323
sandaru <sandaru20240016@iit.ac.lk>
2026-02-21 12:47:27 +0530

    log in button

--- frontend/src/components/loginButton.jsx
+++ frontend/src/components/loginButton.jsx
@@ -1,4 +1,4 @@
-// Reusable Auth Buttons
+
 export const LoginButton = ({ onClick }) => (
 	<button
 		className="ml-8 px-6 py-2 rounded-full bg-white/10 border border-white/30 text-white font-semibold hover:bg-white/20 transition-all duration-200"
