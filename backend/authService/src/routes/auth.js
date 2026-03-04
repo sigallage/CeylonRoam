ed551c1186a61211329917260b4223c8930218d4
goagoonewardena <oshadhigoon@gmail.com>
2026-03-04 17:21:22 +0530

    Replace verifyToken middleware with protect middleware for the /profile update route

--- backend/authService/src/routes/auth.js
+++ backend/authService/src/routes/auth.js
@@ -1,9 +1,12 @@
+
 const express = require("express");
 const authController = require('../../controllers/authController');
+const protect = require('../middleware/protect');
 const router = express.Router();
+
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
-router.put("/profile", verifyToken, authController.updateProfile);
+router.put("/profile", protect, authController.updateProfile);
 
 module.exports = router;
